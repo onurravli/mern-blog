7d7700f2b999ed0000b6e23886bf1108a048e053
onurravli <onurravli@hotmail.com>
2024-01-13 11:56:49 +0300

    chore(middlewares/joi): update middleware

--- packages/backend/src/middlewares/joi.middleware.ts
+++ packages/backend/src/middlewares/joi.middleware.ts
@@ -5,43 +5,77 @@ import JoiImport from "joi";
 const joiImport = JoiImport.extend(DateExtension) as typeof JoiImport;
 
 const schemes = {
-  params: joiImport.object({
+  getById: joiImport.object({
     id: joiImport.string().length(24).required(),
   }),
-  body: joiImport.object({
+  deleteById: joiImport.object({
+    id: joiImport.string().length(24).required(),
+  }),
+  postBody: joiImport.object({
     title: JoiImport.string().required().max(50),
     content: JoiImport.array().required(),
     author: JoiImport.object({
       name: JoiImport.string().required(),
       mail: JoiImport.string().email().required(),
     }).required(),
-    created_on: JoiImport.date().required(),
-    updated_on: JoiImport.date().required(),
+  }),
+  updateBody: joiImport.object({
+    title: JoiImport.string().max(50),
+    content: JoiImport.array(),
+    author: JoiImport.object({
+      name: JoiImport.string(),
+      mail: JoiImport.string().email(),
+    }),
+  }),
+  updateParams: JoiImport.object({
+    id: joiImport.string().length(24).required(),
   }),
 };
 
 const joi = (req: Request, res: Response, next: NextFunction) => {
   switch (req.method) {
     case "GET":
-    case "DELETE":
-      var { error } = schemes.params.validate(req.params);
+      var { error } = schemes.getById.validate(req.params);
       if (error) {
         return res.status(400).json({
           error: error.message,
         });
+      } else {
+        next();
+      }
+      break;
+    case "DELETE":
+      var { error } = schemes.deleteById.validate(req.params);
+      if (error) {
+        return res.status(400).json({
+          error: error.message,
+        });
+      } else {
+        next();
       }
       break;
     case "PUT":
-    case "POST":
-      var { error } = schemes.body.validate(req.body);
+      var { error } = schemes.updateBody.validate(req.body);
+      var { error } = schemes.updateParams.validate(req.params);
       if (error) {
         return res.status(400).json({
           error: error.message,
         });
+      } else {
+        next();
+      }
+      break;
+    case "POST":
+      var { error } = schemes.postBody.validate(req.body);
+      if (error) {
+        return res.status(400).json({
+          error: error.message,
+        });
+      } else {
+        next();
       }
       break;
   }
-  next();
 };
 
 export default joi;
